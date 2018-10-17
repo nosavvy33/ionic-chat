ae9e4363b66b5b8dacf61bb383b4a2221fa970b9
Douglas Franklin <douglasfkln@outlook.com>
2018-10-17 17:38:38 -0300

    code pages rooms and add-rooms

--- src/pages/add-room/add-room.ts
+++ src/pages/add-room/add-room.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase';
 
 /**
  * Generated class for the AddRoomPage page.
@@ -15,11 +16,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddRoomPage {
 
+  data = { roomname:'' };
+  ref = firebase.database().ref('chatrooms/');
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddRoomPage');
   }
-
+  
+  addRoom() {
+    let newData = this.ref.push();
+    newData.set({
+      roomname:this.data.roomname
+    });
+    this.navCtrl.pop();
+  }
 }
