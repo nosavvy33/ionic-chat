0b732b96cf622f3631fa0c20843b3731d784fa3f
Douglas Franklin <douglasfkln@outlook.com>
2018-10-17 17:11:40 -0300

    create and config google firebase, add page signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase'
 
 import { HomePage } from '../pages/home/home';
+
+const config = {
+  apiKey: "AIzaSyAt_4sGgAs4BP2AH5e_FqI9Yxxu6jTR4-s",
+  authDomain: "chat-ionic3-firebase.firebaseapp.com",
+  databaseURL: "https://chat-ionic3-firebase.firebaseio.com",
+  projectId: "chat-ionic3-firebase",
+  storageBucket: "chat-ionic3-firebase.appspot.com",
+  messagingSenderId: "187596625088"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,6 +28,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
 
